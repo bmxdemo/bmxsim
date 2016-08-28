f70db0bd108a58e35484af2b08d3acb182e9d1e0
Anze Slosar <slosar@gmail.com>
2016-08-28 08:54:27 -0400

    Fixed bugs and added plotting routine. A quick run should be something like:
    (dt=180 for it to run quickly)
    driver/mockStream.py --dt=180 -o test.pickle
    driver/plotStream.py  ./test.pickle

---
+++ driver/plotStream.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import numpy as np
+import sys,pylab
+sys.path.append('py/')
+import DataStream
+from BMXDemo import BMXDemoSingleDish
+import cPickle as cP
+from optparse import OptionParser
+import matplotlib.pyplot as plt
+
+parser = OptionParser(usage="Usage: %prog [options] picke_filename")
+parser.add_option("-o", "--output", dest="outfile", default=None,
+                  help="Output filename, if none plot to screen", type="string")
+# default
+for option in parser.option_list:
+    if option.default != ("NO", "DEFAULT"):
+        option.help += (" " if option.help else "") + "[default: %default]"
+(o, args) = parser.parse_args()
+if (len(args)!=1):
+    parser.print_help()
+    sys.exit(0)
+
+st=cP.load(open(args[0]))
+plt.figure(figsize=(10,10))
+plt.ylabel("freq [MHz]")
+plt.xlabel("t [h]")
+plt.imshow(st.streams[0],interpolation='nearest',extent=(0.,st.tMax_h(),st.nuMin(), st.nuMax()),aspect='auto')
+plt.colorbar()
+if (o.outfile is None):
+    plt.show()
+else:
+    plt.savefig(o.outfile)
