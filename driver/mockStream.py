334d5fc27c311bafa3076b2985bfd4acc803f23e
evanjarena <evanjarena@gmail.com>
2016-10-07 00:51:45 -0400

    New Option
    Made which field we simulate selectable by option and fixed typo in line 16

--- driver/mockStream.py
+++ driver/mockStream.py
@@ -13,8 +13,10 @@ parser = OptionParser()
 parser.add_option("--dt", dest="dt", default=10,
                   help="dt for timesteps (in seconds!)", type="float")
 parser.add_option("--tmax", dest="tmax_h", default=1.,
-                  help="Lenght of observation (in hours!)", type="float")
-parser.add_option("-o", "--output", dest="outfile", default="testStream.pickle",
+                  help="Length of observation (in hours!)", type="float")
+parser.add_option("--field", dest="whichfield",default="cosmo",
+                  help="Which field to simulate; to wit, cosmo, ptso, gfree, or egfree", type="string") 
+parser.add_option("-o","--output", dest="outfile", default="testStream.pickle",
                   help="Output filename", type="string")
 # default
 for option in parser.option_list:
@@ -25,5 +27,5 @@ for option in parser.option_list:
 telescope=BMXDemoSingleDish()
 tlist=DataStream.getTimeList(dt=o.dt, Ns=int((o.tmax_h*3600.)/o.dt)+1)
 stream=DataStream.DataStream(telescope,tlist=tlist)
-stream.fillStream()
+stream.fillStream(reader=None,whichfield=o.whichfield,Npix=201,Nfwhm=3)
 cP.dump(stream,open(o.outfile,'w'))
