210889f824251f1c13d9a8b980993d54a29e18a7
Christopher Sheehy <chris.d.sheehy@gmail.com>
2018-04-30 16:33:31 -0400

    allow change of nu_f

--- bmxsim/BMXDemo.py
+++ bmxsim/BMXDemo.py
@@ -5,18 +5,19 @@ from astropy.coordinates import AltAz, EarthLocation
 import astropy.units as u
 from TelescopeBase import TelescopeBase
 from BeamAiry import BeamAiry
+import numpy as np
 
-class BMXDemoSingleDish (TelescopeBase):
+class BMXDemoSingleDish(TelescopeBase):
     """
     A single dish in the basin
     """
-    def __init__(self, airy=True):
+    def __init__(self, airy=True, nu_f=800.0):
         """ Constructor
             if airy==True, use airy disk approximation to the system
         """
-        # 4 meter dish pointing at zenith, fully illuminated at 800MHz
-        beams=[BeamAiry(0,0,90.,0,4.,800.)]
-        TelescopeBase.__init__(self, beams, 800., 1400.,
+        # 3.5 meter dish pointing at zenith 
+        beams=[BeamAiry(0, 0, 90., 0, 3.5, nu_f)]
+        TelescopeBase.__init__(self, beams, 
                                EarthLocation(lat=40.87792*u.deg, lon=-72.85852*u.deg, height=0*u.m),
                                "BMXDemoSingleDish")
 
