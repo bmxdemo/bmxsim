83b9a563c93ddc93bb9238526a08209fb6750837
Christopher Sheehy <chris.d.sheehy@gmail.com>
2018-04-30 16:37:28 -0400

    get rid of unused nuparams

--- bmxsim/TelescopeBase.py
+++ bmxsim/TelescopeBase.py
@@ -9,20 +9,15 @@ class TelescopeBase(object):
      but 
 
     """
-    def __init__(self, beams, numin, numax, location=None, name="telescope"):
+    def __init__(self, beams, location=None, name="telescope"):
         """ Constructor:
         beams -- list of BeamBase derived objects representing beams of this telescope
         numin,numax -- min and max frequency of the telescope in MHz
         location -- telescope location, astropy Earth Location 
         """
         self.beams=beams
-        self.numin=float(numin)
-        self.numax=float(numax)
         self.location=location
         self.name=name
 
-    def setNuParams(self, numin, numax):
-        self.numin=numin
-        self.numax=numax
         
     
